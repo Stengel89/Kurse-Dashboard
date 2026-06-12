a046455d941fb8e25b288e973ed8fa312d1be2a8
Stengel89 <louis.j.stengel@gmail.com>
2026-06-12 18:11:04 +0200

    a

--- script.js
+++ script.js
@@ -1,46 +1,12 @@
-async function ladeKurse() {
-  const response = await fetch(
-    "https://api.frankfurter.dev/v1/latest?base=USD&symbols=EUR,THB,CNY,MYR"
-  );
+console.log("JavaScript läuft");
 
-  const data = await response.json();
+const tbody = document.querySelector("#data-table tbody");
 
-  const tbody = document.querySelector("#data-table tbody");
-  tbody.innerHTML = "";
+const row = document.createElement("tr");
 
-  const kurse = [
-    {
-      name: "EUR/USD",
-      value: 1 / data.rates.EUR
-    },
-    {
-      name: "USD/THB",
-      value: data.rates.THB
-    },
-    {
-      name: "USD/CNY",
-      value: data.rates.CNY
-    },
-    {
-      name: "USD/MYR",
-      value: data.rates.MYR
-    },
-    {
-      name: "EUR/CNY",
-      value: data.rates.CNY / data.rates.EUR
-    }
-  ];
+row.innerHTML = `
+  <td>TEST</td>
+  <td>1234</td>
+`;
 
-  kurse.forEach(kurs => {
-    const row = document.createElement("tr");
-
-    row.innerHTML = `
-      <td>${kurs.name}</td>
-      <td>${kurs.value.toFixed(4)}</td>
-    `;
-
-    tbody.appendChild(row);
-  });
-}
-
-ladeKurse();
+tbody.appendChild(row);
